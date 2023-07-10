559fd63be2732b2928afa824d8aed7afaaa3ced6
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-07-10 20:59:18 -0300

    added tv series posters and descriptions

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+        node: true,
+    },
+    extends: [
+        'react-app',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier', 'react-hooks'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/prop-types': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import { options } from './var';
+import Search from './pages/Search';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [listmovies, setListmovies] = useState([]);
+    const imageURL = 'https://image.tmdb.org/t/p/w500/';
+    const info = options;
+
+    const getMovies = async () => {
+        const movies = fetch(
+            'https://api.themoviedb.org/3/tv/top_rated?language=pt-BR&page=1',
+            info,
+        );
+
+        const [contents] = await Promise.all([movies]);
+        const postjson = await contents.json();
+        setListmovies(postjson.results);
+        console.log(typeof listmovies);
+        console.log(listmovies[0]);
+    };
+
+    return (
+        <section>
+            <div className="LoadMovies">
+                <h1>Carregar Movies</h1>
+                <button onClick={getMovies}>load</button>
+            </div>
+            <Search />
+            <div className="Box">
+                {listmovies.map((value, index) => {
+                    return (
+                        <div className="Movies" key={index}>
+                            <h1>{value.name}</h1>
+                            <img src={imageURL + value.poster_path} alt="a" />
+                            <p>{value.overview}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
 }
 
 export default App;
