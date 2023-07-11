63302b6175f5b5dfd8abe4e889400181bd7d7aaa
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-07-10 21:26:18 -0300

    next series tv page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Search from './pages/Search';
 
 function App() {
     const [listmovies, setListmovies] = useState([]);
+    const [pageNow, setpageNow] = useState(1);
+
     const imageURL = 'https://image.tmdb.org/t/p/w500/';
     const info = options;
 
     const getMovies = async () => {
         const movies = fetch(
-            'https://api.themoviedb.org/3/tv/top_rated?language=pt-BR&page=1',
+            `https://api.themoviedb.org/3/tv/top_rated?language=pt-BR&page=${pageNow}`,
             info,
         );
 
@@ -21,11 +23,24 @@ function App() {
         console.log(listmovies[0]);
     };
 
+    const nextPageb = () => {
+        setpageNow(pageNow + 1);
+        console.log(pageNow);
+    };
+    const nextPagea = () => {
+        setpageNow(pageNow - 1);
+        console.log(pageNow);
+    };
+
     return (
         <section>
             <div className="LoadMovies">
                 <h1>Carregar Movies</h1>
                 <button onClick={getMovies}>load</button>
+            </div>
+            <div className="nextPage">
+                <button onClick={nextPagea}>{'<'}</button>
+                <button onClick={nextPageb}>{'>'}</button>
             </div>
             <Search />
             <div className="Box">
