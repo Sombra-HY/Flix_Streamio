09e2ba14c5e354b6cb4b285ab803b56a312bb3e8
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-08 20:15:35 -0300

    InputSearch

--- src/components/InputSearch/InputSearch.jsx
+++ src/components/InputSearch/InputSearch.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { URLTMDB, options } from '../../var';
 import fetch_api_json from '../../utils/FetchApiJson';
-import { GridContent } from '../GridContent/GridContent';
 
-export const InputSearch = () => {
+import './style.css';
+import { useNavigate } from 'react-router-dom';
+
+export const InputSearch = ({ listseach, SetListseach }) => {
     const [inputvalue, SetInputValue] = useState('');
-    const [listseach, SetListseach] = useState([]);
-    const [listsSuggestions, SetlistsSuggestions] = useState([]);
+    const [valueSearch, setValueSearch] = useState('');
     const { search } = URLTMDB;
+    const navigate = useNavigate();
 
     const searchMidias = async () => {
         const wordSearch = search.movie + inputvalue;
@@ -15,49 +17,34 @@ export const InputSearch = () => {
 
         const SearchMidias = await fetch_api_json(wordSearch, options);
 
-        SetListseach(SearchMidias);
-        console.log(SearchMidias);
-    };
-    const suggestionsInput = async () => {
-        if (inputvalue === '') {
-            return SetlistsSuggestions([]);
-        }
+        SetListseach(SearchMidias.results);
+        console.log(SearchMidias.results);
+        navigate('/searchmidias');
 
-        const urlSuggestions = search.keyword + inputvalue;
-        const SearchMidias = await fetch_api_json(urlSuggestions, options);
-        SetlistsSuggestions(SearchMidias);
     };
 
     const getValueinput = (e) => {
-        suggestionsInput();
         SetInputValue(e.target.value);
     };
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            searchMidias();
+            setValueSearch(event.target.value);
+
+            valueSearch !== inputvalue && searchMidias();
         }
     };
 
     return (
-        <header>
+        <>
             <input
+                className='inputSearch'
                 type="search"
                 name=""
-                id=""
                 placeholder="Digite"
                 onChange={getValueinput}
                 onKeyDown={handleKeyDown}
             />
-            <ul className="List-suggestions">
-                {listsSuggestions.map((obj) => {
-                    console.log(obj.name);
-                    return <li>{obj.name}</li>;
-                })}
-            </ul>
-
-            <p>{`pesquisa: ${inputvalue}`}</p>
-            {listseach.length != 0 && <GridContent content={listseach} />}
-        </header>
+        </>
     );
 };
