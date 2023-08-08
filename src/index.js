826d93600a9b53e728d4ce0c1ea66e4ca8c4e995
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-08 20:18:06 -0300

    Rotes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App></App>);
+root.render(<App />);
