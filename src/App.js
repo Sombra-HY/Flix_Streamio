521733399c7693b4106bfaf362faef7372c8f51a
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-10 11:52:27 -0300

    0.1.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
@@ -13,47 +13,46 @@ function App() {
     const [listseach, SetListseach] = useState([]);
     return (
         <>
-            <BrowserRouter>
-                <header>
-                    <nav className="nav-container">
-                        <Link to="/">Home</Link>
-                        <Link to="/series">Series</Link>
-                        <Link to="/">Movies</Link>
-                        <InputSearch
-                            listseach={listseach}
-                            SetListseach={SetListseach}
-                        />
-                    </nav>
-                </header>
-                <main>
-                    {/*{listseach.length !== 0 && (*/}
-                    {/*    <GridContent content={listseach} />*/}
-                    {/*)}*/}
-                </main>
+            <header>
+                <nav className="nav-container">
+                    <Link to="/">Home</Link>
+                    <Link to="/series">Series</Link>
+                    <Link to="/">Movies</Link>
+                    <InputSearch
+                        listseach={listseach}
+                        SetListseach={SetListseach}
+                    />
+                </nav>
+            </header>
+            <main>
+                {/*{listseach.length !== 0 && (*/}
+                {/*    <GridContent content={listseach} />*/}
+                {/*)}*/}
+            </main>
 
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    {listseach.length !== 0 && (
-                        <Route
-                            path="/searchmidias"
-                            element={<GridContent content={listseach} />}
-                        />
-                    )}
-                    <Route path="/serie/:id" element={<PageMidia />} />
-                    <Route path="/movie/:id" element={<PageMidia />} />
-                </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Home />} />
+                {listseach.length !== 0 && (
+                    <Route
+                        path="/searchmidias"
+                        element={<GridContent content={listseach} />}
+                    />
+                )}
+                <Route path="/serie/:id" element={<PageMidia />} />
+                <Route path="/movie/:id" element={<PageMidia />} />
+            </Routes>
 
-                <footer>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />a
-                </footer>
-            </BrowserRouter>
+            <footer>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />a
+            </footer>
         </>
     );
 }
