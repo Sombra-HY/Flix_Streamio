c54858a9e4ce446f8f346e4694353dd1cdb49139
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-09 21:18:39 -0300

    0.1.0

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
