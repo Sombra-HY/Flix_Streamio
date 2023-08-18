2e239f392fd9a6ff6fe3c3eef6a3d3526cda7b95
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-18 12:49:10 -0300

    style for moblie

---
+++ src/components/TextExpandable/TextExpandable.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const TextExpandable = ({ text, maxlength }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const Maxlength = maxlength || 100;
+
+    const expandabledText = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <>
+            <div className="Conteiner-expladeble">
+                {text.length > Maxlength && !isExpanded ? (
+                    <p onClick={expandabledText}>
+                        {text.slice(0, Maxlength) + '...'}
+                    </p>
+                ) : (
+                    <p onClick={expandabledText}>{text}</p>
+                )}
+            </div>
+        </>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { Home2 } from './pages/Home/Home2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App />
+        <Home2 />
     </BrowserRouter>,
 );
