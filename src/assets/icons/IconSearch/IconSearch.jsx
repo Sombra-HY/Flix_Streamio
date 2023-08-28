dd261a67473efa57cde4e3eef7f9dd6e06ad6e3b
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-08-28 12:11:37 -0300

    IconSearch.jsx update logic

--- src/assets/icons/IconSearch/IconSearch.jsx
+++ src/assets/icons/IconSearch/IconSearch.jsx
@@ -1,11 +1,11 @@
 import Search from './Search_icons.png';
 import Searchchecked from './Search_checked_icon.png';
 
-export const IconSearch = ({ check = true, className = null }) => {
+export const IconSearch = ({ check = false, className = null }) => {
     return (
         <img
             className={className}
-            src={check ? Search : Searchchecked}
+            src={check ? Searchchecked : Search}
             alt="iconSearch"
         />
     );
