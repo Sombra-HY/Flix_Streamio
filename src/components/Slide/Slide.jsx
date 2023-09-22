2b2e571cc34c75f0aae0adde95f1010d39deded2
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-09-22 11:05:51 -0300

    LocalStorage imgs home/slide

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-
 import './style.css';
 import { getImgMovie } from './data/getImgMovie';
 import { useChangeImage } from './hooks/useChangeImage';
@@ -9,26 +8,36 @@ export const Slide = () => {
     const [currentImageIndex] = useChangeImage(img);
 
     useEffect(() => {
-        getImgMovie().then((data) => {
-            console.log(data);
-            setImgs(data);
-        });
+        const storedImgs = localStorage.getItem('imgs');
+
+        if (!storedImgs) {
+            getImgMovie().then((data) => {
+                localStorage.setItem('imgs', JSON.stringify(data));
+                setImgs(data);
+            });
+        } else {
+            try {
+                const parsedImgs = JSON.parse(storedImgs);
+                setImgs(parsedImgs);
+            } catch (error) {
+                console.error('Erro ao analisar JSON:', error);
+            }
+        }
     }, []);
 
     return (
         <article className="ContentSlides">
-            {img.length > 0 &&
-                img.map((image, index) => (
-                    <figure
-                        key={index}
-                        style={{
-                            opacity: index === currentImageIndex ? 1 : 0,
-                            transition: 'opacity 0.5s ease-in-out',
-                        }}
-                    >
-                        <img src={image} alt={`Slide ${index}`} />
-                    </figure>
-                ))}
+            {img.map((image, index) => (
+                <figure
+                    key={index}
+                    style={{
+                        opacity: index === currentImageIndex ? 1 : 0,
+                        transition: 'opacity 0.5s ease-in-out',
+                    }}
+                >
+                    <img src={image} alt={`Slide ${index}`} />
+                </figure>
+            ))}
         </article>
     );
 };
